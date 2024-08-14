9173f8737365e82ef149e6d14fbe5fd833b17f63
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-08-14 21:04:26 +0100

    Commit

--- home-manager/configurations.nix
+++ home-manager/configurations.nix
@@ -12,4 +12,16 @@
     userEmail = "100799838+Robert-M-Lucas@users.noreply.github.com";
     lfs.enable = true;
   };
+
+  programs.fish.enable = true;  
+  # Use fish without using it as the login shell as fish is not POSIX compliantt
+  # programs.bash = {
+  #   interactiveShellInit = ''
+  #     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+  #     then
+  #       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+  #       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+  #     fi
+  #   '';
+  # };
 }
