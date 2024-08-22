387fc3f8b7390ec82047f7647a9131eb22ab5b89
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-08-22 02:39:04 +0100

    Script improvements

--- home-manager/scripts/nix-config.sh
+++ home-manager/scripts/nix-config.sh
@@ -1,13 +1,32 @@
 cd ~/nix-config
+
+echo "| Git pull"
 git pull
+
 codium -w .
+
+echo "| Git add"
 git add *
-echo "Enter hostname to switch to:"
+
+echo "> Enter hostname to switch to:"
 read hostname
+
+echo "> Update flake? (y/N):"
+read user_input
+if [ "$user_input" == "y" || "$user_input" == "Y" ]; then
+    echo "| Updating flake"
+    nix flake update
+fi
+
+echo "| Switching"
 sudo nixos-rebuild --flake .#$hostname switch
-read -p "Press enter to git diff"
+
+read -p "> Press enter to git diff"
 git diff
-echo "Enter commit message:"
+
+echo "> Enter commit message:"
 read commit_msg
 git commit -a -m "$commit_msg"
+
+echo "Git push"
 git push
