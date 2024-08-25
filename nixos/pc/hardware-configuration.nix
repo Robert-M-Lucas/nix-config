842eb334ade5ae8fd48623c55870b065995ff207
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-08-25 20:48:35 +0100

    nvidia

--- nixos/pc/hardware-configuration.nix
+++ nixos/pc/hardware-configuration.nix
@@ -47,10 +47,10 @@
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
-    nvidiaSettings = false;
+    nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   fileSystems."/" =
