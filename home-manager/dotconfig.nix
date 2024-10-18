cfbe490fc5a8f88f7a1782371ae69f75b4499c2c
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-10-18 19:19:36 +0100

    Commit

--- home-manager/dotconfig.nix
+++ home-manager/dotconfig.nix
@@ -3,5 +3,6 @@
 }
 :
 {
-  xdg.configFile.nvim.source = ./nvim;
+  xdg.configFile.nvim.source = ./dotfiles/nvim;
+  xdg.configFile."JetBrains/PyCharm2024.2/pycharm64.vmoptions".source = ./dotfiles/JetBrains/PyCharm2024.2/pycharm64.vmoptions;
 }
