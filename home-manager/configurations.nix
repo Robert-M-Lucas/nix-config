c0471ba6843999f33c329e94213ff50d527287eb
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-12-24 18:24:40 +0000

    Console theming

--- home-manager/configurations.nix
+++ home-manager/configurations.nix
@@ -53,8 +53,8 @@
             export RGH=\"https://github.com/Robert-M-Lucas\"
             export SHS=/home/robert/nix-config/shells
             export RUST_SHELL=/home/robert/nix-config/shells/rust.nix
-            tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=Yes
-        ";
+            tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Many icons' --transient=Yes
+            ";
     };
 
     programs.vscode = {
