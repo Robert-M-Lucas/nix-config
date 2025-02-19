20a22133f62847ea140c85b6e56ee50ef8ffa42f
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-02-19 02:11:50 +0000

    -

--- shells/rust.nix
+++ shells/rust.nix
@@ -27,6 +27,22 @@ pkgs.mkShell rec {
         alsa-lib
         libudev-zero
         cairo
+        gdk-pixbuf
+        graphene
+        gtk4
+        libadwaita
+
+        gst_all_1.gstreamer
+        # Common plugins like "filesrc" to combine within e.g. gst-launch
+        gst_all_1.gst-plugins-base
+        # Specialized plugins separated by quality
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        # Plugins to reuse ffmpeg to play almost every video format
+        gst_all_1.gst-libav
+        # Support the Video Audio (Hardware) Acceleration API
+        gst_all_1.gst-vaapi
 
         SDL2
 
