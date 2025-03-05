3d55dafc3e15b2d06a39701ff23910b46295219b
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-03-05 22:37:36 +0000

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -285,9 +285,9 @@ in
     programs.spicetify = {
         enable = true;
         enabledExtensions = with spicePkgs.extensions; [
-        adblockify
-        hidePodcasts
-        shuffle # shuffle+ (special characters are sanitized out of extension names)
+            adblockify
+            shuffle
+            oneko
         ];
         theme = spicePkgs.themes.catppuccin;
         colorScheme = "mocha";
