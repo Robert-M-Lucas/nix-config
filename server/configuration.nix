6c5a66e52bee2247771a78c2b6a361f63ffb69ea
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-04-04 01:44:44 +0100

    -

--- server/configuration.nix
+++ server/configuration.nix
@@ -36,6 +36,8 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
+
 
   services.printing.enable = true;
 
