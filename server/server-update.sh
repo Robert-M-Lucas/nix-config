fa47d6faf2f7ba2c0dba52c5269e9946699e4703
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-04-04 01:15:03 +0100

    -

--- server/server-update.sh
+++ server/server-update.sh
@@ -1,11 +1,19 @@
 sudo echo Sudo acquired
 
 cd ~/nix-config
+
+echo "| git pull"
 git pull
 
+echo "| sudo cp ./server/configuration.nix /etc/nixos/configuration.nix"
 sudo cp ./server/configuration.nix /etc/nixos/configuration.nix
+
+echo "| sudo nixos-rebuild switch"
 sudo nixos-rebuild switch
 
+echo "| git add -A"
 git add -A
+echo "| git commit -a"
 git commit -a -m "-"
+echo "| git push"
 git push
