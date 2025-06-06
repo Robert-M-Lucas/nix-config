eea78d78be2c5763a83c615bea21f6c1d5f11ee5
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-06-06 11:44:21 +0100

    -

--- home-manager/dotconfig.nix
+++ home-manager/dotconfig.nix
@@ -1,7 +1,7 @@
 {...}
 : {
   # xdg.configFile.nvim.source = ./dotfiles/nvim;
-  xdg.configFile."JetBrains/PyCharm2024.2/pycharm64.vmoptions".source = ./dotfiles/JetBrains/PyCharm2024.2/pycharm64.vmoptions;
+  # xdg.configFile."JetBrains/PyCharm2024.2/pycharm64.vmoptions".source = ./dotfiles/JetBrains/PyCharm2024.2/pycharm64.vmoptions;
   xdg.configFile."fastfetch/logo.txt".source = ./dotfiles/fastfetch/logo.txt;
   xdg.configFile."fastfetch/config.jsonc".source = ./dotfiles/fastfetch/config.jsonc;
 }
