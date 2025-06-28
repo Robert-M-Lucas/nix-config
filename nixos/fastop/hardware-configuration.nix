61b009c28967233cdd740634668e374514634627
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-06-28 23:12:04 +0100

    -

--- nixos/fastop/hardware-configuration.nix
+++ nixos/fastop/hardware-configuration.nix
@@ -16,7 +16,7 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd" "v4l2loopback"];
+  boot.kernelModules = ["kvm-amd" "v4l2loopback" "i2c-dev" "ddcci_backlight"];
   boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
     perf
@@ -26,6 +26,10 @@
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
   security.polkit.enable = true;
+  services.udev.extraRules = ''
+    SUBSYSTEM=="i2c-dev", KERNEL=="i2c-[0-9]*", ATTRS{class}=="0x030000", TAG+="uaccess"
+    SUBSYSTEM=="dri", KERNEL=="card[0-9]*", TAG+="uaccess"
+  '';
 
   hardware.graphics = {
     enable = true;
