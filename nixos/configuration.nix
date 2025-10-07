39a42df7cc759ea350ea4ebc753652b4de864ca3
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-10-08 00:33:59 +0100

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -175,14 +175,14 @@ in {
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.excludePackages = [pkgs.xterm];
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [pkgs.xterm];
 
   # Enable plasma
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
