c1ac91919f0c56543382baefcefe63090a756bc9
= <=>
2025-11-07 16:11:30 +0000

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,10 +79,9 @@ in {
 
   boot.loader.grub = {
     enable = true;
-    useOSProber = false;
+    useOSProber = true;
     devices = ["nodev"];
     efiSupport = true;
-    configurationLimit = 1;
 
     extraEntries = ''
       menuentry "UEFI Settings" {
@@ -100,17 +99,17 @@ in {
     #   background = "background_options/1.8  - [Classic Minecraft].png";
     #   boot-options-count = 5;
     # };
-    theme = pkgs.stdenv.mkDerivation {
-      pname = "distro-grub-themes";
-      version = "3.2";
-      src = pkgs.fetchFromGitHub {
-        owner = "AdisonCavani";
-        repo = "distro-grub-themes";
-        rev = "v3.1";
-        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-      };
-      installPhase = "cp -r customize/nixos $out";
-    };
+    # theme = pkgs.stdenv.mkDerivation {
+    #  pname = "distro-grub-themes";
+    #  version = "3.2";
+    #  src = pkgs.fetchFromGitHub {
+    #    owner = "AdisonCavani";
+    #    repo = "distro-grub-themes";
+    #    rev = "v3.1";
+    #    hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+    #  };
+    #  installPhase = "cp -r customize/nixos $out";
+    #};
   };
 
   swapDevices = [
