e409331c5013fc3260983ffaa5a4199c9e28680e
= <=>
2025-11-10 10:23:06 +0000

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -370,7 +370,7 @@ in {
   };
 
   programs.steam = {
-    enable = true;
+    enable = !is-worktop;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
