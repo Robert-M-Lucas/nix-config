c86eaed25f004b793fe57813bf3a97bee9e50016
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-11-14 22:23:34 +0000

    Update

--- server/configuration.nix
+++ server/configuration.nix
@@ -1,210 +1,210 @@
 let
   unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-  in
-{
-  config,
-  pkgs,
-  ...
-}: {
-  nixpkgs.config = {
-    packageOverrides = pkgs: {
-      unstable = import unstableTarball {
-        config = config.nixpkgs.config;
+in
+  {
+    config,
+    pkgs,
+    ...
+  }: {
+    nixpkgs.config = {
+      packageOverrides = pkgs: {
+        unstable = import unstableTarball {
+          config = config.nixpkgs.config;
+        };
       };
     };
-  };
 
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos-server";
-
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/London";
-
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-  services.xserver.xkb = {
-    layout = "gb";
-    variant = "";
-  };
-  console.keyMap = "uk";
-
-  # services.xserver.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.displayManager.gdm.autoSuspend = false;
-
-  # programs.dconf.enable = true;
-
-  # services.printing.enable = true;
-
-  # hardware.pulseaudio.enable = false;
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  # };
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  users.users.robert = {
-    isNormalUser = true;
-    description = "Robert Lucas";
-    extraGroups = ["wheel"];
-    packages = with pkgs; [
-      (writeShellScriptBin "nix-clean" (builtins.readFile /home/robert/nix-config/home-manager/scripts/nix-clean.sh))
-      (writeShellScriptBin "server-update" (builtins.readFile /home/robert/nix-config/server/server-update.sh))
-      (writeShellScriptBin "doff" (builtins.readFile /home/robert/nix-config/server/doff.sh))
+    imports = [
+      ./hardware-configuration.nix
     ];
-  };
 
-  nixpkgs.config.allowUnfree = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-      set fish_greeting
-    '';
-  };
+    networking.hostName = "nixos-server";
 
-  # environment.gnome.excludePackages = with pkgs; [
-  #   gnome-tour
-  #   gnome-connections
-  #   epiphany
-  #   geary
-  #   yelp
-  #   seahorse
-  #   gnome-clocks
-  #   gnome-maps
-  #   gnome-weather
-  # ];
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    gh
-    tmux
-    # google-chrome
-    # vscode
-    python3
-    diff-so-fancy
-    fastfetch
-    rustup
-    htop
-    cockpit
-    immich 
-  ];
+    networking.networkmanager.enable = true;
 
-  services.openssh.enable = true;
-  services.openssh.settings = {
-    PermitRootLogin = "no";
-    PasswordAuthentication = true;
-    MaxAuthTries = 6;
-  };
+    time.timeZone = "Europe/London";
 
-  services.cockpit = {
-    enable = true;
-    openFirewall = true;
-  };
+    i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+    services.xserver.xkb = {
+      layout = "gb";
+      variant = "";
+    };
+    console.keyMap = "uk";
 
-  services.tlp.enable = true;
-  powerManagement.powertop.enable = true;
+    # services.xserver.enable = true;
+    # services.xserver.displayManager.gdm.enable = true;
+    # services.xserver.desktopManager.gnome.enable = true;
+    # services.xserver.displayManager.gdm.autoSuspend = false;
 
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "client"; # acts as client only
-    openFirewall = true; # open Tailscale ports
-  };
+    # programs.dconf.enable = true;
 
-  services.samba = {
-    enable = true;
-    openFirewall = true; # still needed for LAN/Tailscale access
-    settings = {
-      data = {
-        path = "/home/robert/data";
-        browseable = true;
-        "valid users" = "robert";
-        "read only" = false;
-        "guest ok" = false;
-        "create mask" = "0644";
-        "directory mask" = "0755";
+    # services.printing.enable = true;
+
+    # hardware.pulseaudio.enable = false;
+    # security.rtkit.enable = true;
+    # services.pipewire = {
+    #   enable = true;
+    #   alsa.enable = true;
+    #   alsa.support32Bit = true;
+    #   pulse.enable = true;
+    # };
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    users.users.robert = {
+      isNormalUser = true;
+      description = "Robert Lucas";
+      extraGroups = ["wheel"];
+      packages = with pkgs; [
+        (writeShellScriptBin "nix-clean" (builtins.readFile /home/robert/nix-config/home-manager/scripts/nix-clean.sh))
+        (writeShellScriptBin "server-update" (builtins.readFile /home/robert/nix-config/server/server-update.sh))
+        (writeShellScriptBin "doff" (builtins.readFile /home/robert/nix-config/server/doff.sh))
+      ];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.bash = {
+      interactiveShellInit = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+        set fish_greeting
+      '';
+    };
+
+    # environment.gnome.excludePackages = with pkgs; [
+    #   gnome-tour
+    #   gnome-connections
+    #   epiphany
+    #   geary
+    #   yelp
+    #   seahorse
+    #   gnome-clocks
+    #   gnome-maps
+    #   gnome-weather
+    # ];
+    environment.systemPackages = with pkgs; [
+      wget
+      git
+      gh
+      tmux
+      # google-chrome
+      # vscode
+      python3
+      diff-so-fancy
+      fastfetch
+      rustup
+      htop
+      cockpit
+      immich
+    ];
+
+    services.openssh.enable = true;
+    services.openssh.settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+      MaxAuthTries = 6;
+    };
+
+    services.cockpit = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    services.tlp.enable = true;
+    powerManagement.powertop.enable = true;
+
+    services.tailscale = {
+      enable = true;
+      useRoutingFeatures = "client"; # acts as client only
+      openFirewall = true; # open Tailscale ports
+    };
+
+    services.samba = {
+      enable = true;
+      openFirewall = true; # still needed for LAN/Tailscale access
+      settings = {
+        data = {
+          path = "/home/robert/data";
+          browseable = true;
+          "valid users" = "robert";
+          "read only" = false;
+          "guest ok" = false;
+          "create mask" = "0644";
+          "directory mask" = "0755";
+        };
       };
     };
-  };
 
-  services.immich = {
-    package = pkgs.unstable.immich;
-    enable = true;
-    port = 2283;
-    openFirewall = true;
-    host = "0.0.0.0";
-  };
-  # services.immich.accelerationDevices = null;
+    services.immich = {
+      package = pkgs.unstable.immich;
+      enable = true;
+      port = 2283;
+      openFirewall = true;
+      host = "0.0.0.0";
+    };
+    # services.immich.accelerationDevices = null;
 
-  users.users.immich.extraGroups = [ "video" "render" ];
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-    ];
-  };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+    users.users.immich.extraGroups = ["video" "render"];
+    hardware.graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+      ];
+    };
+    environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
 
-  boot.supportedFilesystems = ["nfs"];
+    boot.supportedFilesystems = ["nfs"];
 
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-  };
+    services.samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # # --- NFS ---
-  # services.nfs.server = {
-  #   enable = true;
-  #   exports = ''
-  #     /data 192.168.1.0/24(rw,fsid=0,nohide,insecure,no_subtree_check) 100.64.0.0/10(rw,fsid=0,nohide,insecure,no_subtree_check)
-  #   '';
+    # # --- NFS ---
+    # services.nfs.server = {
+    #   enable = true;
+    #   exports = ''
+    #     /data 192.168.1.0/24(rw,fsid=0,nohide,insecure,no_subtree_check) 100.64.0.0/10(rw,fsid=0,nohide,insecure,no_subtree_check)
+    #   '';
 
-  #   lockdPort = 4001;
-  #   mountdPort = 4002;
-  #   statdPort = 4000;
-  #   extraNfsdConfig = '''';
+    #   lockdPort = 4001;
+    #   mountdPort = 4002;
+    #   statdPort = 4000;
+    #   extraNfsdConfig = '''';
 
-  #   # mountdPort = 20048;
-  #   # statdport = 32765;
-  #   # lockdPort = 32767;
-  # };
+    #   # mountdPort = 20048;
+    #   # statdport = 32765;
+    #   # lockdPort = 32767;
+    # };
 
-  networking.firewall = {
-    enable = true;
-    allowedUDPPorts = [41641];
-    allowedTCPPorts = [ 80 443 2283 ];
-    # allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
-    # allowedUDPPorts = [ 111 2049 4000 4001  4002 41641 20048 ];
-    trustedInterfaces = ["tailscale0"]; # trust VPN
-  };
+    networking.firewall = {
+      enable = true;
+      allowedUDPPorts = [41641];
+      allowedTCPPorts = [80 443 2283];
+      # allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
+      # allowedUDPPorts = [ 111 2049 4000 4001  4002 41641 20048 ];
+      trustedInterfaces = ["tailscale0"]; # trust VPN
+    };
 
-  security.polkit.enable = true;
+    security.polkit.enable = true;
 
-  system.stateVersion = "24.11";
-}
+    system.stateVersion = "24.11";
+  }
