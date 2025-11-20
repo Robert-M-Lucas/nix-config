6913fe6f487a6921884558920c99438b60582e34
= <=>
2025-11-20 10:17:54 +0000

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -338,7 +338,6 @@ in {
   console.enable = false;
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
-    gnome-connections
     epiphany # web browser
     geary # email reader
     yelp # help viewer
