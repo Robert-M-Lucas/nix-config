af8524266e62b53f58b54ebc3c9bd430004080ae
Robert Lucas <rlucas@triomotion.com>
2026-02-19 12:30:44 +0000

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -84,21 +84,21 @@ in {
       set locale_dir=
     '';
 
-    # theme =
-    #   if is-worktop
-    #   then null
-    #   else
-    #     pkgs.stdenv.mkDerivation {
-    #       pname = "distro-grub-themes";
-    #       version = "3.2";
-    #       src = pkgs.fetchFromGitHub {
-    #         owner = "AdisonCavani";
-    #         repo = "distro-grub-themes";
-    #         rev = "v3.1";
-    #         hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-    #       };
-    #       installPhase = "cp -r customize/nixos $out";
-    #     };
+    theme =
+      # if is-worktop
+      # then null
+      # else
+        pkgs.stdenv.mkDerivation {
+          pname = "distro-grub-themes";
+          version = "3.2";
+          src = pkgs.fetchFromGitHub {
+            owner = "AdisonCavani";
+            repo = "distro-grub-themes";
+            rev = "v3.1";
+            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+          };
+          installPhase = "cp -r customize/nixos $out";
+        };
   };
 
   swapDevices = [
