cfa55b889b51135dbd0851f60bd9e08b6d493519
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2026-04-10 22:54:33 +0100

    -

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -373,7 +373,7 @@ in {
         if is-wsl
         then []
         else [
-          winboat-fix
+          # winboat-fix
           # Dolphin
           kdePackages.dolphin
           kdePackages.qtsvg
